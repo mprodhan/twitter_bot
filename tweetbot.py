d2653652579bac48f78574631cd095e12b197d92
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-12-01 23:09:50 -0500

    an attempt to favorite a tweet. Need to override error.

--- tweetbot.py
+++ tweetbot.py
@@ -5,11 +5,21 @@ auth.set_access_token('1328206757416546306-4O5oLNq6uek0ElR2RIN5iat2RTJMkD', 'xMT
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-
 user = api.me()
 # print(user.name)
 # print(user.screen_name)
 # print(user.followers_count)
+
+search_string = 'crypto'
+numberOfTweets = 3
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print("Favorited")
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+# tech debt: need to over ride the Read-Only application cannot POST error.
